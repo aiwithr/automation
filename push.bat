340544d7d00ff2c7a196cc81ab041e8b455f0f5e
Rakibul Hassan <rakibul.hassan@link3.net>
2023-09-30 13:04:37 +0600

    Github Actions commit from VScode

---
+++ push.bat
@@ -0,0 +1,8 @@
+git init
+git add -A
+git commit -m "Github Actions commit from VScode"
+:: git remote add origin git@aiwithr:aiwithr/aiwithr.github.io.git
+:: git remote add origin https://aiwithr@github.com/aiwithr.github.io.git
+git remote add origin https://github.com/aiwithr/automation.git 
+git push origin main --force
+:: git push -u origin master
